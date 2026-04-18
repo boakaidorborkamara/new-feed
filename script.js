5117ddcd08dadb36c1ba09c7ecbb41c6eca86e8d
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2026-04-18 13:35:57 +0000

    fetch new data and display in console

--- script.js
+++ script.js
@@ -1,43 +1,68 @@
 // f75d58d59ee64f508d8c0a27862ea597
+let api_key = "pub_2a6a6d3b545a4bc2b5afe031f938fbe5";
+let base_url = "https://newsdata.io/api/1/latest";
+
+// https://newsdata.io/api/1/latest?apikey=pub_2a6a6d3b545a4bc2b5afe031f938fbe5
 // https://newsapi.org/docs/endpoints/everythinghttps://newsapi.org/docs/endpoints/everything
 
 let cards_container = document.getElementById("cards-container");
 let news_section = document.getElementById("news-section");
-let cta_section = document.getElementById("cta-container")
+let cta_section = document.getElementById("cta-container");
 
 let state = {
-    news : [],
-    setNews(new_state){
-        this.new = [...new_state]
+  news: [],
+  setState(new_state) {
+    this.new = {...new_state};
+    renderUI();
+  },
+};
+
+async function fetchData(url) {
+  console.log("url", url);
+
+  try {
+    let response = await fetch(url, {
+      method: "GET",
+      headers: { "X-ACCESS-KEY": api_key },
+    });
+
+    if(!response.ok){
+        throw new Error()
     }
+
+    let data = await response.json();
+
+    console.log(data);
+
+    return data;
+  } catch (err) {
+    console.log("error", err);
+  }
+
 }
 
-
-function fetchData(){
-    fetch(url)
-    .then(response => response.json())
-    .then((data)=>{
-        return data;
-    })
-}
-
-function renderUI(){
-    if(state.news.length === 0){
-        let err_html = `
+function renderUI() {
+  if (state.news.length === 0) {
+    let err_html = `
             <div class="error-text-container">
                 <p class="error-text">No news found!</p>
             </div>
-        `
+        `;
 
-        news_section.insertAdjacentHTML("beforeend", err_html)
+    news_section.insertAdjacentHTML("beforeend", err_html);
 
-        // cta_section.classList.add("display")
-        cards_container.innerHTML = "";
-    }
-
+    // cta_section.classList.add("display")
+    cards_container.innerHTML = "";
+  }
 }
 
+window.addEventListener("load", async () => {
 
-window.addEventListener("load", ()=>{
-    renderUI();
-})
+  let news = await fetchData(base_url);
+
+  state.setState(news);
+
+  console.log("news", news);
+
+
+});
